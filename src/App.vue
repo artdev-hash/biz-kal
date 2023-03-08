1382f8df140817260e1c8f9953c83426d5752c1d
artdev-hash <kim.dev.pr@gmail.com>
2023-03-08 14:25:21 +0800

    biz

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,11 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="@github.com/artdev-hash" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/about">About Biz-kal</RouterLink>
       </nav>
     </div>
   </header>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,12 +9,7 @@ defineProps({
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+    <h2 class="green">{{ msg }}</h2>
   </div>
 </template>
 
@@ -30,6 +25,9 @@ h3 {
 }
 
 .greetings h1,
+.greetings h2{
+  text-align:center;
+}
 .greetings h3 {
   text-align: center;
 }
