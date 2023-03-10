7467b08a65b5b5dbd4e2e56c839e375858022463
artdev-hash <kim.dev.pr@gmail.com>
2023-03-10 21:39:38 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+# if you are deploying to a custom domain
+#echo 'www.lbiz-kal.com' > CNAME
+
+git init
+git add -A 
+git commit -m 'New Deployment'
+
+git push -f git@github.com:artdev-hash/biz-kal.git master:gh-pages
+
+cd -
+
