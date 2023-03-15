e123c0d7a3a75f90ddb657b9126f4a34c7737e77
artdev-hash <kim.dev.pr@gmail.com>
2023-03-15 19:22:52 +0800

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import PageNotFound from '../views/ErrorNotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,8 +25,12 @@ const router = createRouter({
     },
     {
       // path: "*",
-        path: '/:catchAll(.*)*',
-        redirect: '/'
+      path: "/:catchAll(.*)",
+      name: "NotFound",
+      component: PageNotFound,
+      meta: {
+        requiresAuth: false
+      }
     }
   ]
 
