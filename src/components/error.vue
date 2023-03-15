ea43c1d3079130e88ff43f4d4970d938399fb623
artdev-hash <kim.dev.pr@gmail.com>
2023-03-15 19:42:50 +0800

    fix

---
+++ src/components/error.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+      <div style="font-size: 30vh">
+        404
+      </div>
+
+      <div class="text-h2" style="opacity:.4">
+        Oops. Nothing here...
+      </div>
+    </div>
+</template>
