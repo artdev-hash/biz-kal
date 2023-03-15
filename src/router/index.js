25e49425884eac1056f39f223d20a53dfcb0ab47
artdev-hash <kim.dev.pr@gmail.com>
2023-03-15 18:14:10 +0800

    fixing

--- src/router/index.js
+++ src/router/index.js
@@ -25,7 +25,7 @@ const router = createRouter({
     {
       // path: "*",
         path: '/:catchAll(.*)*',
-        component: () => import('../views/ErrorNotFound.vue')
+        redirect: '/'
     }
   ]
 
