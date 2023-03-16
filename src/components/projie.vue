d61211b00290a76e7c0781c956073706e62d278d
artdev-hash <kim.dev.pr@gmail.com>
2023-03-16 14:48:43 +0800

    proj

--- src/components/projie.vue
+++ src/components/projie.vue
@@ -14,9 +14,9 @@ import DocumentationIcon from './icons/IconDocumentation.vue'
     <code>- <a href="https://devs.flutter.ph/#/" target="_blank" rel="noopener">devs</a></code> <br>
     <code>- <a href="https://artdev-hash.github.io/krprt-octo-uploader/" target="_blank" rel="noopener">krpt-octo-uploader</a></code> <br>
     <code>- <a href="https://artdev-hash.github.io/kshared-nocto-copier/" target="_blank" rel="noopener">kshared-nocto-copier</a></code> <br>
-    <code>- <a href="" target="_blank" rel="noopener">PLANBANK</a></code> <br>
-    <code>- <a href="" target="_blank" rel="noopener">CQMS</a></code><br>
-    <code>- <a href="" target="_blank" rel="noopener">LMS</a></code><br>
+    <code>- <a href="">PLANBANK</a></code> <br>
+    <code>- <a href="">CQMS</a></code><br>
+    <code>- <a href="">LMS</a></code><br>
     <code>- <a href="https://artdev-hash.github.io/vbex-dot.net/" target="_blank" rel="noopener">vbex-dot.net</a></code>
 
   </icns>
