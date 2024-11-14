c23c1bc782f38efcfb30d740ebae8bbe6a3d3084
artdev-hash <kim.dev.pr@gmail.com>
2024-11-14 22:35:15 +0800

    rmscloud

--- src/components/about.vue
+++ src/components/about.vue
@@ -18,8 +18,7 @@ import CopyWright from './CopyWright.vue'
     <div class="typewriter-effect">
     <div class="text" id="typewriter-text"></div>
     <code>* You can reach Me @<a href="mailto:dev@lorenzokimarthur.com" target="_blank" rel="noopener">dev</a></code></div><br><br>
-    <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1746810261&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/echoevolution" title="EchoEvolution" target="_blank" style="color: #cccccc; text-decoration: none;">EchoEvolution</a> · <a href="https://soundcloud.com/echoevolution/beautiful-things-benson-boone" title="Beautiful Things - Benson Boone (EchoEvolution Mashup)" target="_blank" style="color: #cccccc; text-decoration: none;">Beautiful Things - Benson Boone (EchoEvolution Mashup)</a></div>
-    
+
 
 
     </icns>
