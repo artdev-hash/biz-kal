affeeeac125abc836c9fb11964070715b3a67198
artdev-hash <kim.dev.pr@gmail.com>
2024-11-14 21:04:33 +0800

    revise

--- src/components/about.vue
+++ src/components/about.vue
@@ -9,13 +9,19 @@ import CopyWright from './CopyWright.vue'
       <DocumentationIcon />
      </template>
     <template #heading>👨‍💻 About Me</template>
-    <code>A FullStack Developer</code><br>
-    <code>Status: <b>Available / Open for Commissions</b></code><br>
-    <code>Currently: <b>on Open-Source, Projects, Assignments and Layout</b></code><br>
+    <code>* I'm a IT also a Developer</code><br>
+    <code>* Available & <b>Open for Commissions</b></code><br>
+    <code>* Have Done <b>Open-Source Contribution, Some Projects, Tech Support, Typing and Layouts</b></code><br>
+    <code>* Have Experience in Tech, Banking, Health and Accounting kind of Job</code><br>
+    <code>* I would love to hear from you.. Let's Talk about Tech, Finance and Investing..</code><br>
+    <code>* Thank you for reading my Profile..</code><br>
     <div class="typewriter-effect">
     <div class="text" id="typewriter-text"></div>
-    <code>Contact Me: <a href="mailto:dev@lorenzokimarthur.com" target="_blank" rel="noopener">dev</a></code>
-    </div>
+    <code>* You can reach Me @<a href="mailto:dev@lorenzokimarthur.com" target="_blank" rel="noopener">dev</a></code></div><br><br>
+    <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1746810261&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/echoevolution" title="EchoEvolution" target="_blank" style="color: #cccccc; text-decoration: none;">EchoEvolution</a> · <a href="https://soundcloud.com/echoevolution/beautiful-things-benson-boone" title="Beautiful Things - Benson Boone (EchoEvolution Mashup)" target="_blank" style="color: #cccccc; text-decoration: none;">Beautiful Things - Benson Boone (EchoEvolution Mashup)</a></div>
+    
+
+
     </icns>
     <br><br><br><br>
     <CopyWright />
--- src/components/projie.vue
+++ src/components/projie.vue
@@ -11,25 +11,28 @@ import CopyWright from './CopyWright.vue'
       <DocumentationIcon />
     </template>
     <template #heading><code>🚀 Projects and 🛰️ Collaborations</code></template>
-    <code>Recent  💡  >></code><br>
+    <code>Recently  💡  >></code><br>
     <div class="typewriter-effect">
     <div class="text" id="typewriter-text"></div>
-    <code>- <a href="https://m-k-products.github.io/store/" target="_blank" rel="noopener">M.K PRODUCTS</a></code></div>
+    <code>- Participated on HACKTOBER 24</code></div>
+    <code>- A member of PICSPRO</code><br>
+    <code>- <a href="https://m-k-products.github.io/store/" target="_blank" rel="noopener">M.K PRODUCTS</a></code><br>
     <code>- <a href="https://artdev-hash.github.io/english-tagalog-vocab/" target="_blank" rel="noopener">ENGLISH TAGALOG VOCAB</a></code><br>
     <code>- <a href="https://artdev-hash.github.io/pc-upgrade-and-repair/" target="_blank" rel="noopener">PC UPGRADE AND REPAIR</a></code><br>
-    <code>- Participation in HACKTOBER 23</code><br>
     <code>- <a href="https://artdev-hash.github.io/bicol_translate/" target="_blank" rel="noopener">BICOL TRANSLATE</a></code><br>
-    <code>- <a href="https://artdev-hash.github.io/octo-feedboard/" target="_blank" rel="noopener">OCTO-FEEDBOARD</a></code><br>
+    <code>- Participated on HACKTOBER 23</code><br>
     <code>- <a href="https://lorenzokimarthur.com" target="_blank" rel="noopener">LORENZOKIMARTHUR.COM</a></code><br>
-    <code>- Participation in HACKTOBER 22</code><br>
-    <code>- <a href="https://artdev-hash.github.io/biz-kal/" target="_blank" rel="noopener">BIZ-KAL</a></code><br>
-    <code>- <a href="https://devs.flutter.ph/" target="_blank" rel="noopener">FLUTTER DEVS</a></code> <br>
-    <code>- <a href="https://artdev-hash.github.io/snamegenerator/" target="_blank" rel="noopener">SNAMEGENERATOR</a></code><br>
-    <code>- Participation in 30 Days Flutter 21</code><br>
+    <code>- Template <a href="https://artdev-hash.github.io/octo-feedboard/" target="_blank" rel="noopener">OCTO-FEEDBOARD</a></code><br>
+    <code>- Profile <a href="https://artdev-hash.github.io/biz-kal/" target="_blank" rel="noopener">BIZ-KAL</a></code><br>
+    <code>- Tree Planting Certificate</code><br>
+    <code>- Completed HACKTOBER 22</code><br>
+    <code>- Contributed to <a href="https://devs.flutter.ph/" target="_blank" rel="noopener">FLUTTER DEVS</a></code> <br>
+    <code>- A Flutter app named <a href="https://artdev-hash.github.io/snamegenerator/" target="_blank" rel="noopener">SNAMEGENERATOR</a></code><br>
+    <code>- Completed 30 Days of Flutter 21</code><br>
     <code>- <a href="https://artdev-hash.github.io/krprt-octo-uploader/" target="_blank" rel="noopener">KRPRT-OCTO-UPLOADER</a></code><br>
     <code>- <a href="https://artdev-hash.github.io/kshared-nocto-copier/" target="_blank" rel="noopener">KSHARED-NOCTO-COPIER</a></code><br>
-    <code>- PLANBANK website</code><br>
-    <code>- IAMTHECOFFEEGUY</code><br>
+    <code>- Update a Banking website </code><br>
+    <code>- A Wordpress named IAMTHECOFFEEGUY</code><br>
     <code>- CQMS - Customer Queuing Management System</code><br>
     <code>- LMS - Library Management System</code><br>
     <code>- <a href="https://artdev-hash.github.io/vbex-dotnet/" target="_blank" rel="noopener">VBEX-DOT.NET</a></code>
@@ -40,11 +43,11 @@ import CopyWright from './CopyWright.vue'
       <SupportIcon />
     </template>
     <template #heading>👨‍💻 Support My Actions by</template>
-    <code> * Sending Inspirational Messages 💌</code><br>
-    <code> * Liking or Following My Pages:<br></code>
+    <code> * Giving Stars ⭐ to my Github Repositories</code><br>
+    <code> * You can also <a href="https://www.buymeacoffee.com/kimdevprc" target="_blank" title="artdev-hash">BuyMeaCoffee ☕</a></code><br>
+    <code> * Also by Liking or Following My Socials:<br></code>
     <code>  - YT <a href="https://www.youtube.com/channel/UCP2psTOak3Z60eQj1wq--cg" target="_blank" rel="noopener">@kimlorenzo143</a></code><br>
     <code>  - FB <a href="https://www.facebook.com/kimITservices" target="_blank" rel="noopener">kimITservices</a></code><br>
-    <code> * <a href="https://www.buymeacoffee.com/kimdevprc" target="_blank" title="artdev-hash">BuyMeaCoffee ☕</a></code><br>
     <code> ** THANK YOU FOR YOUR SUPPORT ❤️ **</code>
     <br><br></icns>
   <br><br><br><br>
